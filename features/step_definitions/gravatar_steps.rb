57070d543d616db903906b7eda419e5f79b441dd
palaciosgabriel@hotmail.com <palaciosgabriel@hotmail.com>
2016-10-23 21:13:00 -0300

    primer step acceder home page

---
+++ features/step_definitions/gravatar_steps.rb
@@ -0,0 +1,21 @@
+Given(/^i am a user$/) do
+    @primero = User.first
+    @primero.email
+	should match("offerer@test.com")
+end
+
+Given(/^I am logged$/) do
+   visit '/login'
+  fill_in('user[email]', :with => 'offerer@test.com')
+  fill_in('user[password]', :with => 'Passw0rd!')
+  click_button('Login')
+  page.should have_content('offerer@test.com')
+end
+
+When(/^I access to the home page$/) do
+  visit '/'
+end
+
+Then(/^i can see my gravatar at the top page$/) do
+  pending # express the regexp above with the code you wish you had
+end
