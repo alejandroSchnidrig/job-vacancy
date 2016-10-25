8ded76dcc03514d4143754b92fef874c889c5fb9
alejandroSchnidrig <ale.schnidrig@gmail.com>
2016-10-25 01:06:58 +0000

    pass first step

--- features/step_definitions/job_share_steps.rb
+++ features/step_definitions/job_share_steps.rb
@@ -1,5 +1,10 @@
 Given(/^that exist a job offer$/) do
-  pending 
+  @job_offer = JobOffer.new
+  @job_offer.owner = User.first
+  @job_offer.title = 'Chef'
+  @job_offer.location = 'Caseros'
+  @job_offer.description = 'Excellent pay'
+  @job_offer.save
 end
 
 When(/^I share the offer$/) do
