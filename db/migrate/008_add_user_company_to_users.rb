3860a54cf72cff509f13ce94d5a927d3e8803d50
Matias <tute.mdm@gmail.com>
2016-10-25 15:38:46 -0300

    update and delete innecesaries files

---
+++ db/migrate/008_add_user_company_to_users.rb
@@ -0,0 +1,13 @@
+migration 9, :add_user_company_to_users do
+  up do
+    modify_table :users do
+      add_column :company, String
+    end
+  end
+
+  down do
+    modify_table :users do
+      drop_column :company
+    end
+  end
+end
