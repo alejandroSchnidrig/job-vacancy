3239572bb3b328e6f4968c153920908208f5e42e
alejandroSchnidrig <ale.schnidrig@gmail.com>
2016-10-25 01:00:15 +0000

    added job-share feature and steps definitions

---
+++ features/step_definitions/job_share_steps.rb
@@ -0,0 +1,11 @@
+Given(/^that exist a job offer$/) do
+  pending 
+end
+
+When(/^I share the offer$/) do
+  pending 
+end
+
+Then(/^the person should receive a mail with the offer info$/) do
+  pending 
+end
