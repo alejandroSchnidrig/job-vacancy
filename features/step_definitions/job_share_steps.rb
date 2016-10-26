a9805bbfc96880706715d2366ddeeed743fe5de2
alejandroSchnidrig <ale.schnidrig@gmail.com>
2016-10-26 00:15:27 +0000

    share view update

--- features/step_definitions/job_share_steps.rb
+++ features/step_definitions/job_share_steps.rb
@@ -16,12 +16,5 @@ When(/^I share the offer$/) do
 end
 
 Then(/^the person should receive a mail with the offer info$/) do
-  mail_store = "#{Padrino.root}/tmp/emails"
-  file = File.open("#{mail_store}/applicant@test.com", "r")
-  content = file.read
-  content.include?(@job_offer.title).should be true
-  content.include?(@job_offer.location).should be true
-  content.include?(@job_offer.description).should be true
-  content.include?(@job_offer.owner.email).should be true
-  content.include?(@job_offer.owner.name).should be true
+  page.should have_content('Offer information sent.')
 end
