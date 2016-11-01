a0c3b6fd6cadc0d9df53bb1172006458ab1df91d
alejandroSchnidrig <ale.schnidrig@gmail.com>
2016-11-01 01:39:45 +0000

    added search_by_field definition steps

--- features/step_definitions/search_by_field_steps.rb
+++ features/step_definitions/search_by_field_steps.rb
@@ -1,23 +1,26 @@
 When(/^I do an specific search for title field$/) do
-  pending
+  fill_in('q', :with => 'title: Web Programmer')
+  click_button('search')
 end
 
 Then(/^i can see the offer by its title$/) do
-  pending 
+  page.should have_content('Web Programmer')
 end  
 
 When(/^I do an specific search for location field$/) do
-  pending 
+  fill_in('q', :with => 'location: a nice job')
+  click_button('search')
 end
 
 Then(/^i can see the offer by its location$/) do
-  pending 
+  page.should have_content('a nice job') 
 end
 
 When(/^I do an specific search for description field$/) do
-  pending 
+  fill_in('q', :with => 'description: good pay')
+  click_button('search') 
 end
 
 Then(/^i can see the offer by its description$/) do
-  pending 
+  page.should have_content('good pay')
 end
