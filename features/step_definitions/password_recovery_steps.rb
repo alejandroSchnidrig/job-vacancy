7b21e9fba33b72fe045951915b7cdabad05f0ddd
alejandroSchnidrig <ale.schnidrig@gmail.com>
2016-11-06 21:39:24 +0000

    added password recovery steps_definition file

---
+++ features/step_definitions/password_recovery_steps.rb
@@ -0,0 +1,23 @@
+Given(/^that i am a user$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I access the login page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I click on the recovery password button$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I access to the password recovery page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I put my email address$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^i can see an email with new password$/) do
+  pending # express the regexp above with the code you wish you had
+end
