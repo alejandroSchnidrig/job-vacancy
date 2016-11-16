f35f9f9d395259513d88f2b45131a2232c0ebef1
palaciosgabriel@hotmail.com <palaciosgabriel@hotmail.com>
2016-11-15 23:48:32 -0300

    add gps_coordinates_steps and features

---
+++ features/gps_coordinates.feature
@@ -0,0 +1,15 @@
+Feature: Find Near offers to same location
+In order to search multiple offers in Near Location
+As a user
+I want to Find Near offers to same location
+
+
+Background:
+    Given that exists 3 jobs offers (2 with similar gps coordinates)
+
+
+Scenario: Find Near offers to same location
+Given I access the offers list page
+When I click Find Near in an offer
+Then i can see the two offers with same location
+Then i can not see the offer with the another location
---
+++ features/step_definitions/gps_coordinates_steps.rb
@@ -0,0 +1,46 @@
+Given(/^that exists (\d+) jobs offers \((\d+) with similar gps coordinates\)$/) do |arg1, arg2|
+  JobOffer.all.destroy
+  @job_offer_one = JobOffer.new
+  @job_offer_two = JobOffer.new
+  @job_offer_three = JobOffer.new
+
+  @job_offer_one.owner = User.first
+  @job_offer_two.owner = User.first
+  @job_offer_three.owner = User.first
+
+  @job_offer_one.title = 'chef'
+  @job_offer_one.location = 'caseros'
+  @job_offer_one.description = 'french food'
+  @job_offer_one.latitude = -37.5
+  @job_offer_one.longitude = -58.8
+  @job_offer_one.save
+
+  @job_offer_two.title = 'president'
+  @job_offer_two.location = 'usa'
+  @job_offer_two.description = 'like trump'
+  @job_offer_one.latitude = -37.4
+  @job_offer_one.longitude = -58.9
+  @job_offer_two.save
+
+  @job_offer_three.title = 'actor'
+  @job_offer_three.location = 'caseros'
+  @job_offer_three.description = 'good as robert de niro'
+  @job_offer_one.latitude = -36.5
+  @job_offer_one.longitude = -58.8
+  @job_offer_three.save
+end
+
+When(/^I click Find Near in an offer$/) do
+  all(:xpath, "(//a[text()='Find Near'])")[0].click
+end
+
+Then(/^i can see the two offers with same location$/) do
+  page.should have_content('caseros') 
+  page.should have_content('chef')
+  page.should have_content('actor')
+end
+
+Then(/^i can not see the offer with the another location$/) do
+  page.should have_no_content('usa')
+end
+
